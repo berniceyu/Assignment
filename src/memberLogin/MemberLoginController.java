1504588507c7516addce9cec6b3c2cdb38a61556
Bernice <ywyber@gmail.com>
2017-05-03 01:07:11 +1000

    Try to commit
    Bernice 03/05/2017
    - Added "Staff Sign In" Page
    - Rename folder
    - Deleted "forgot password" hyperlink

---
+++ src/memberLogin/MemberLoginController.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package memberLogin;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author Yu Bernice
+ */
+public class MemberLoginController implements Initializable {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private TextField email;
+    
+    @FXML
+    private PasswordField password;
+    
+    @FXML
+    private Button signinbtn;
+    
+    @FXML
+    void signin(ActionEvent event) {
+        String userEmail = email.getText();
+        String userPass = password.getText();
+        System.out.println(userEmail + userPass);
+        if (userEmail.equals("123@gmail.com") && userPass.equals("pass"))
+        {
+            label.setText("Welcome");
+        }
+        else {
+                label.setText("Username or Password invalid");
+        }
+        }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
